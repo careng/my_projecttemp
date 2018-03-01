2e19c34e12c8783be1537c1cf6bfb26b1d68b77e
Caren Goldberg <caren.goldberg@wsu.edu>
2018-03-01 10:14:47 -0800

    change py to oceania

--- analysis.py
+++ analysis.py
@@ -7,5 +7,5 @@ Created on Thu Mar  1 10:02:53 2018
 #importing the package pandas and call it pd then use pd to read in the data
 
 import pandas as pd
-dat = pd.read_csv('data/gapminder_gdp_europe.csv')
+dat = pd.read_csv('data/gapminder_gdp_oceania.csv')
 
