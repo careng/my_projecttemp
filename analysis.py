9d6c0c6a4255a9fd8d601e61db7257a1d4467836
Caren Goldberg <caren.goldberg@wsu.edu>
2018-03-01 10:12:06 -0800

    made a py doc

---
+++ analysis.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  1 10:02:53 2018
+
+@author: caren
+"""
+#importing the package pandas and call it pd then use pd to read in the data
+
+import pandas as pd
+dat = pd.read_csv('data/gapminder_gdp_europe.csv')
+
